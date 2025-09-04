3721bdb104aa5ff36914cb5278680e1851adf8fa
Abdurrahman Ramadhan <abdmandhan@gmail.com>
2025-09-04 18:45:49 +0700

    Integrate Prisma library with initial setup, including schema and seed script. Update package dependencies and workspace configuration to include new libraries.

---
+++ libs/prisma/README.md
@@ -0,0 +1,11 @@
+# prisma
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build prisma` to build the library.
+
+nx run prisma:seed
+nx run prisma:migrate
+nx run prisma:generate
---
+++ libs/prisma/src/seed.ts
@@ -0,0 +1,40 @@
+import prisma from "./index.js";
+import bcryptjs from "bcryptjs";
+
+const main = async () => {
+  const users = [
+    {
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "password",
+    },
+  ];
+
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+
+    await prisma.users.upsert({
+      where: { email: user.email },
+      create: {
+        name: user.name,
+        email: user.email,
+        password: bcryptjs.hashSync(user.password, 10),
+      },
+      update: {
+        name: user.name,
+        password: bcryptjs.hashSync(user.password, 10),
+      },
+    });
+  }
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    console.log("Seed complete");
+  })
+  .catch(async (e) => {
+    console.error("Seed failed:", e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
